51e9a9822619c3ddd3365afcd78f22e5ca91b6b7
Yasmin Fazelinia <yfazelinia@gmail.com>
2013-11-28 14:26:05 -0600

    Add calculator; bug for taxable doestic items

--- taxquestion.rb
+++ taxquestion.rb
@@ -1,16 +1,14 @@
 class Item
   # create item and pass in arguments- quanity and price
   # Optional args: taxable and import; defaults are false
-  attr_accessor :price, :taxable, :imported
-    def initialize(price, taxable= false, imported= false)
-      # @quantity = quantity
+  attr_reader :name, :price, :taxable, :imported
+    def initialize(name, price, taxable= false, imported= false)
+      @name = name
       @price = price
       @taxable = taxable
       @imported = imported
     end
-  # book1 = Item.new(12.49)
-  # impchoc = Item.new(10.00, false, true)
-  # impperfume = Item.new(47.50, true, true)
+
 
 end
 
@@ -30,11 +28,13 @@ class ShoppingCart
     @items << item
     end
   end
+end
 
+class Calculator
   # Calculate the total price of each item including tax
-  def receipt
+  def calculator_taxed_price(cart)
 
-    self.items.each do |item|
+    cart.items.each do |item|
     # For tax-exempt, non-imported items
       if exempt_domestic?(item)
        exempt_domestic(item)
@@ -54,15 +54,8 @@ class ShoppingCart
   end
 
   # method for rounding
-  
-  def find_cart_quantity(item)
-    @items.count(item)
-  end
 
-  def item_quantity
-    # for receipt will need to only print each unique item once, but multipled by its total_price
-    total_items_price = item.price * self.find_cart_quantity(item)
-  end
+
   # methods for each tax structure
   def exempt_domestic?(item)
     item.taxable == false && item.imported == false
@@ -78,50 +71,66 @@ class ShoppingCart
   end
 
   def exempt_domestic(item)
-    total_tax = 0.0
+    rounded_tax = 0.0
     total_price = item.price
-    puts "#{item} costs #{total_price}"
+    puts "#{item.name} costs #{total_price}"
   end
   def exempt_imported(item)
-    total_tax = item.price * 0.05
-    total_price = item.price + total_tax
-    puts "#{item} costs #{total_price}"
+    raw_tax = (item.price * 5) / 100
+    rounded_tax = (raw_tax * 20).ceil / 20.0
+    total_price = item.price + rounded_tax
+    puts "#{item.name} costs #{total_price}"
   end
   def taxable_domestic(item)
-    total_tax = item.price * 0.10
-    total_price = item.price + total_tax
-    puts "#{item} costs #{total_price}"
+    raw_tax = item.price * 0.10
+    rounded_tax = (raw_tax * 20).ceil / 20.0
+    total_price = item.price + rounded_tax
+    puts "#{item.name} costs #{total_price}"
   end
   def taxable_imported(item)
-    total_tax = item.price * 0.15
-    total_price = item.price + total_tax
-    puts "#{item} costs #{total_price}"
+    raw_tax = (item.price * 15)/100
+    rounded_tax = (raw_tax * 20).ceil / 20.0
+    total_price = item.price + rounded_tax  
+    puts "#{item.name} costs #{total_price}"
   end
   # all are called in a receipt method 
-end
-
-class Calculator
-  # calculate tax
-
+  
 end
 
 
 
 class Receipt
   # print receipt 
-  def purchase
-    #return receipt
+
+  def item_quantity
+    # for receipt will need to only print each unique item once, but multipled by its total_price
+    total_items_price = item.price * self.find_cart_quantity(item)
   end
+  # def find_cart_quantity(item)
+  #   @items.count(item)
+  # end
+  # def find_cart_quantity(cart,item)
+  #   cart.items.count(item)
+  # end
 end
 
-item1 = Item.new(10.0)
-item2 = Item.new(20.0, false, true)
-item3 = Item.new(100.0, true, false)
-item4 = Item.new(500.0, true, true)
+item1_1 = Item.new("book", 12.49)
+item1_2 = Item.new("music CD", 14.99, true, false)
+item1_3 = Item.new("chocolate bar", 0.85)
+item2_1 = Item.new("box of chocolates", 10.00, false, true)
+item2_2 = Item.new("bottle of perfume",47.50, true, true)
+item3_1 = Item.new("bottle of perfume",27.99, true, true)
+item3_2 = Item.new("bottle of perfume", 18.99, true, false)
+item3_3 = Item.new("packet of headache pills", 9.75)
+item3_4 = Item.new("box of chocolates", 11.25, false, true)
 cart1 = ShoppingCart.new
-cart1.add_item_to_cart(item1, 2)
-cart1.add_item_to_cart(item2)
-cart1.add_item_to_cart(item3)
-cart1.add_item_to_cart(item4)
-
+cart1.add_item_to_cart(item1_1)
+cart1.add_item_to_cart(item1_2)
+cart1.add_item_to_cart(item1_3)
+cart1.add_item_to_cart(item2_1)
+cart1.add_item_to_cart(item2_2)
+cart1.add_item_to_cart(item3_1)
+cart1.add_item_to_cart(item3_2)
+cart1.add_item_to_cart(item3_3)
+cart1.add_item_to_cart(item3_4)
 
