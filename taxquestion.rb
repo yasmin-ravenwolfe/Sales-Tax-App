fc4da28b6999343d289fa4051e9422417fdda68d
Yasmin Fazelinia <yfazelinia@gmail.com>
2013-11-28 15:45:03 -0600

    Calculate price in Item class, but via receipt method in Receipt class

--- taxquestion.rb
+++ taxquestion.rb
@@ -1,7 +1,7 @@
 class Item
   # create item and pass in arguments- quanity and price
   # Optional args: taxable and import; defaults are false
-  attr_reader :name, :price, :taxable, :imported
+  attr_reader :name, :price, :taxable, :imported, :rounded_tax, :total_price
     def initialize(name, price, taxable= false, imported= false)
       @name = name
       @price = price
@@ -9,7 +9,68 @@ class Item
       @imported = imported
     end
 
+    # Calculate the total price of each item including tax
+    def calculate_price
 
+    # For tax-exempt, non-imported items
+      if exempt_domestic?
+       exempt_domestic
+       
+      # For tax-exempt, imported items
+      elsif exempt_imported?
+        exempt_imported
+        
+      # For taxed, non-imported items
+      elsif taxable_domestic?
+        taxable_domestic
+     
+      # For taxed, imported items
+      elsif taxable_imported?
+        taxable_imported
+      end    
+    end
+    
+    # helper methods to determine each tax structure
+    def exempt_domestic?  
+      @taxable == false && @imported == false
+    end
+    def exempt_imported?
+      @taxable == false && @imported == true
+    end
+    def taxable_domestic?
+      @taxable == true && @imported == false
+    end
+    def taxable_imported?
+      @taxable == true && @imported == true
+    end
+
+    # helper methods to calculate item price with rounded tax
+    def exempt_domestic
+      @rounded_tax = 0.0
+      @total_price = self.price
+      puts "#{self.name} costs #{total_price}"
+    end
+    def exempt_imported
+      raw_tax = (self.price * 5) / 100
+      @rounded_tax = (raw_tax * 20).ceil / 20.0
+      @total_price = self.price + @rounded_tax
+      puts "#{self.name} costs #{total_price}"
+    end
+    def taxable_domestic
+      raw_tax = (self.price * 10) / 100
+      @rounded_tax = (raw_tax * 20).ceil / 20.0
+      # Because floats use binary numbers, some numbers like 10, will give a random decimal. So, round to 2 decimal places to get correct total price. 
+      # Can use BigDecimal library to avoid; but not in this exercise because it is an external library.
+      @total_price = (self.price + @rounded_tax).round(2)
+      puts "#{self.name} costs #{total_price}"
+      
+    end
+    def taxable_imported
+      raw_tax = (item.price * 15)/100
+      @rounded_tax = (raw_tax * 20).ceil / 20.0
+      @total_price = item.price + @rounded_tax  
+      puts "#{item.name} costs #{total_price}"
+    end
 end
 
 class ShoppingCart
@@ -29,72 +90,11 @@ class ShoppingCart
     end
   end
 
-  # Calculate the total price of each item including tax
-  def calculator
-
-    self.items.each do |item|
-    # For tax-exempt, non-imported items
-      if exempt_domestic?(item)
-       exempt_domestic(item)
-       
-      # For tax-exempt, imported items
-      elsif exempt_imported?(item)
-        exempt_imported(item)
-        
-      # For taxed, non-imported items
-      elsif taxable_domestic?(item)
-        taxable_domestic(item)
-     
-      # For taxed, imported items
-      elsif taxable_imported?(item) 
-        taxable_imported(item)
-      end    
-    end
-  end
+  
 
 
 
-  # helper methods to determine each tax structure
-  def exempt_domestic?(item)
-    item.taxable == false && item.imported == false
-  end
-  def exempt_imported?(item)
-    item.taxable == false && item.imported == true
-  end
-  def taxable_domestic?(item)
-    item.taxable == true && item.imported == false
-  end
-  def taxable_imported?(item)
-    item.taxable == true && item.imported == true
-  end
-
-  # helper methods to calculate item price with rounded tax
-  def exempt_domestic(item)
-    rounded_tax = 0.0
-    total_price = item.price
-    puts "#{item.name} costs #{total_price}"
-  end
-  def exempt_imported(item)
-    raw_tax = (item.price * 5) / 100
-    rounded_tax = (raw_tax * 20).ceil / 20.0
-    total_price = item.price + rounded_tax
-    puts "#{item.name} costs #{total_price}"
-  end
-  def taxable_domestic(item)
-    raw_tax = (item.price * 10) / 100
-    rounded_tax = (raw_tax * 20).ceil / 20.0
-    # Because floats use binary numbers, some numbers like 10, will give a random decimal. So, round to 2 decimal places to get correct total price. 
-    # Can use BigDecimal library to avoid; but not in this exercise because it is an external library.
-    total_price = (item.price + rounded_tax).round(2)
-    puts "#{item.name} costs #{total_price}"
-    
-  end
-  def taxable_imported(item)
-    raw_tax = (item.price * 15)/100
-    rounded_tax = (raw_tax * 20).ceil / 20.0
-    total_price = item.price + rounded_tax  
-    puts "#{item.name} costs #{total_price}"
-  end
+  
   # all are called in a receipt method   
 end
 
@@ -106,18 +106,21 @@ class Receipt
   end
 
   def receipt(cart)
-    
+    cart.items.each do |item|
+      item.calculate_price
+    end
   end
-  def item_quantity
+  def item_quantity(item)
     # for receipt will need to only print each unique item once, but multipled by its total_price
-    total_items_price = item.price * self.find_cart_quantity(item)
+    total_items_price = item.price * cart.items.count(item)
+    # find_cart_quantity(item)
   end
   # def find_cart_quantity(item)
   #   @items.count(item)
   # end
-  # def find_cart_quantity(cart,item)
-  #   cart.items.count(item)
-  # end
+  def find_cart_quantity(cart,item)
+    cart.items.count(item)
+  end
 end
 
 item1_1 = Item.new("book", 12.49)
