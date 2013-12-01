1b5ae095ae8d325d6a9298b536448b202bc9b836
Yasmin Fazelinia <yfazelinia@gmail.com>
2013-12-01 14:03:53 -0600

    All tests but CommandLine working

--- spec/command_line_spec.rb
+++ spec/command_line_spec.rb
@@ -4,24 +4,27 @@ require 'rspec'
 require_relative '../lib/command_line'
 
 
-# module SalesTax
-#   describe CommandLine do 
-#     describe ".initialize" do 
-#       it "returns a receipt for file passed in as argument" do 
+module SalesTax
+  describe CommandLine do 
+    describe ".initialize" do 
+      it "returns a receipt for file passed in as argument" do 
 
-#         files = double('files')
-#         path = double('path')
-#         # line = '1 box of chocolates at 10.00'
-#         item = Item.new("box of foo", 10.00)
-#         shopping_cart = ShoppingCart.new
-#         shopping_cart.add_item_to_cart(item)
-#         result = CommandLine.new(files)
+        ARGV = double('ARGV')
+        files = double('files')
+        path = double('path')
+        ARGV.should_receive(:length).and_return(1)
+        ARGV.should_receive(:each).and_yield(path)
+        # line = '1 box of chocolates at 10.00'
+        item = Item.new("box of foo", 10.00)
+        shopping_cart = ShoppingCart.new
+        shopping_cart.add_item_to_cart(item)
+        result = CommandLine.new(files)
 
-#        expect(result).to eq(shopping_cart.purchase)
-#       end
-#     end
+       expect(result).to eq(shopping_cart.purchase)
+      end
+    end
 
 
 
-#   end
-# end
+  end
+end
