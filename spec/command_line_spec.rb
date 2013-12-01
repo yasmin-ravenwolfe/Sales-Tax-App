0caeda0dea762ad94d37a9d887ffa8038f5d58c5
Yasmin Fazelinia <yfazelinia@gmail.com>
2013-11-30 21:43:20 -0600

    Pass all tests except inventory file and command line

---
+++ spec/command_line_spec.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'rspec'
+require_relative '../lib/command_line'
+
+
+module SalesTax
+  describe CommandLine do 
+    desc
+
+
+
+  end
+end
--- spec/inventory_file_spec.rb
+++ spec/inventory_file_spec.rb
@@ -6,20 +6,23 @@ require_relative '../lib/inventory_file'
 
 module SalesTax
   describe InventoryFile do 
-    let(:inventory_file) {InventoryFile.new("spec/input1.txt")} 
+    let(:inventory_file) {InventoryFile.new(file)} 
+
 
     describe ".initialize" do 
       it "sets the path attribute" do
 
       
 
-       expect(inventory_file.path).to eq("spec/input1.txt")
+       expect(inventory_file.path).to eq(file)
       end
     end
 
     describe "#read" do
       it "reads and parses the file and adds items to cart" do 
-
+        file = mock('file')
+        File.should_receive(:open).with("path", "r").and_yield(file)
+        file.should_receive(:read)
         inventory_file.read 
 
         expect(inventory_file.cart.items.size).to eq(3)
